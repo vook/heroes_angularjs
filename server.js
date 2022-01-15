b27c66a98fa7c5ddd9af045cccc02c86836f9d8d
Jhony Mota <jhony.developer@gmail.com>
2022-01-15 20:35:37 -0300

    Altera configuração do server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 // your angular-project folder
-app.use('/', express.static(__dirname + '/angular-project'));
+app.use('/', express.static(__dirname + '/app'));
 
 app.listen(app.get('port'), function() {
     console.log("running: port", app.get('port'));
